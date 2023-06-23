9f1711a91a0e072059c7359d377dc9672f8ce068
failaip12 <86018517+failaip12@users.noreply.github.com>
2023-06-23 16:17:02 +0200

    Search

--- again/index.php
+++ again/index.php
@@ -2,14 +2,74 @@
 declare(strict_types=1);
 require_once 'core/init.php';
 $db = DB::getInstance();
-$oglasi_query = $db->query("SELECT * FROM `oglasi` WHERE admin_id is NULL");
-if (!Input::exists('get')) {
-    $trenutna_strana = 1;
-} else {
-    $trenutna_strana = (int) Input::get('strana');
+$marka = "%" . strtolower(Input::get('marka')) . "%";
+$model = "%" . strtolower(Input::get('model')) . "%";
+$pogon = "%" . strtolower(Input::get('pogon')) . "%";
+$menjac = "%" . strtolower(Input::get('menjac')) . "%";
+$gorivo = "%" . strtolower(Input::get('gorivo')) . "%";
+
+$godiste_od = !empty(Input::get('godiste_od')) ? Input::get('godiste_od') : PHP_INT_MIN;
+$godiste_do = !empty(Input::get('godiste_do')) ? Input::get('godiste_do') : PHP_INT_MAX;
+$kilometraza_od = !empty(Input::get('kilometraza_od')) ? Input::get('kilometraza_od') : PHP_INT_MIN;
+$kilometraza_do = !empty(Input::get('kilometraza_do')) ? Input::get('kilometraza_do') : PHP_INT_MAX;
+$cena_od = !empty(Input::get('cena_od')) ? Input::get('cena_od') : PHP_INT_MIN;
+$cena_do = !empty(Input::get('cena_do')) ? Input::get('cena_do') : PHP_INT_MAX;
+$kubikaza_od = !empty(Input::get('kubikaza_od')) ? Input::get('kubikaza_od') : PHP_INT_MIN;
+$kubikaza_do = !empty(Input::get('kubikaza_do')) ? Input::get('kubikaza_do') : PHP_INT_MAX;
+$snaga_od = !empty(Input::get('snaga_od')) ? Input::get('snaga_od') : PHP_INT_MIN;
+$snaga_do = !empty(Input::get('snaga_do')) ? Input::get('snaga_do') : PHP_INT_MAX;
+
+$sql = "SELECT * FROM oglasi WHERE admin_id is NOT NULL";
+
+$bindings = [];
+
+if (!empty($marka)) {
+  $sql .= " AND LOWER(marka) LIKE ?";
+  $bindings[] = $marka;
 }
-$broj_oglasa = $oglasi_query->count();
-$oglasi = $db->query('SELECT * FROM oglasi WHERE admin_id is NOT NULL limit ? , ?', array(($trenutna_strana - 1) * 10, 10))->results();
+
+if (!empty($model)) {
+  $sql .= " AND LOWER(model) LIKE ?";
+  $bindings[] = $model;
+}
+
+$sql .= " AND godiste BETWEEN ? AND ?";
+$bindings[] = $godiste_od;
+$bindings[] = $godiste_do;
+
+$sql .= " AND kilometraza BETWEEN ? AND ?";
+$bindings[] = $kilometraza_od;
+$bindings[] = $kilometraza_do;
+
+$sql .= " AND cena BETWEEN ? AND ?";
+$bindings[] = $cena_od;
+$bindings[] = $cena_do;
+
+if (!empty($pogon)) {
+  $sql .= " AND LOWER(pogon) LIKE ?";
+  $bindings[] = $pogon;
+}
+
+if (!empty($menjac)) {
+  $sql .= " AND LOWER(menjac) LIKE ?";
+  $bindings[] = $menjac;
+}
+
+if (!empty($gorivo)) {
+  $sql .= " AND LOWER(gorivo) LIKE ?";
+  $bindings[] = $gorivo;
+}
+
+$sql .= " AND kubikaza BETWEEN ? AND ?";
+$bindings[] = $kubikaza_od;
+$bindings[] = $kubikaza_do;
+
+$sql .= " AND snaga BETWEEN ? AND ?";
+$bindings[] = $snaga_od;
+$bindings[] = $snaga_do;
+
+// Execute the prepared SQL statement
+$oglasi = $db->query($sql, $bindings)->results();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -79,15 +139,52 @@ $oglasi = $db->query('SELECT * FROM oglasi WHERE admin_id is NOT NULL limit ? , 
                     <option value="menjac">Vrsta menjača</option>
                 </select> -->
                 <div class="search-text">
-                    <input type="text" placeholder="Marka" name="marka" class="search-input" />
-                    <input type="text" placeholder="Tip" name="tip" class="search-input" />
-                    <input type="text" placeholder="Godina" name="godina" class="search-input" />
-                    <input type="text" placeholder="Kilometraza" name="kilometraza" class="search-input" />
-                    <input type="text" placeholder="Cena" name="cena" class="search-input" />
-                    <input type="text" placeholder="Pogon" name="pogon" class="search-input" />
-                    <input type="text" placeholder="Menjac" name="menjac" class="search-input" />
-                    
-                    <button class="login-btn">Pretraži</button>
+                <form method="GET">
+                    <input type="text" placeholder="Marka" name="marka" class="search-input" value="<?php echo Input::get('marka', ''); ?>" />
+                    <input type="text" placeholder="Model" name="model" class="search-input" value="<?php echo Input::get('model', ''); ?>" />
+                    <input type="text" placeholder="Godste od" name="godiste_od" class="search-input" value="<?php echo Input::get('godiste_od', ''); ?>" />
+                    <input type="text" placeholder="Godste do" name="godiste_do" class="search-input" value="<?php echo Input::get('godiste_do', ''); ?>" />
+                    <input type="text" placeholder="Kilometraza od" name="kilometraza_od" class="search-input" value="<?php echo Input::get('kilometraza_od', ''); ?>" />
+                    <input type="text" placeholder="Kilometraza do" name="kilometraza_do" class="search-input" value="<?php echo Input::get('kilometraza_do', ''); ?>" />
+                    <input type="text" placeholder="Cena od" name="cena_od" class="search-input" value="<?php echo Input::get('cena_od', ''); ?>" />
+                    <input type="text" placeholder="Cena do" name="cena_do" class="search-input" value="<?php echo Input::get('cena_do', ''); ?>" />
+                    <select id="gorivo" name="gorivo">
+                        <option value="Dizel" <?php if (Input::get('gorivo') === 'Dizel') echo 'selected'; ?>>Dizel</option>
+                        <option value="Benzin" <?php if (Input::get('gorivo') === 'Benzin') echo 'selected'; ?>>Benzin</option>
+                        <option value="Benzin + Gas (TNG)" <?php if (Input::get('gorivo') === 'Benzin + Gas (TNG)') echo 'selected'; ?>>Benzin + Gas (TNG)</option>
+                        <option value="Benzin + Metan (CNG)" <?php if (Input::get('gorivo') === 'Benzin + Metan (CNG)') echo 'selected'; ?>>Benzin + Metan (CNG)</option>
+                        <option value="Električni pogon" <?php if (Input::get('gorivo') === 'Električni pogon') echo 'selected'; ?>>Električni pogon</option>
+                        <option value="Hibridni pogon" <?php if (Input::get('gorivo') === 'Hibridni pogon') echo 'selected'; ?>>Hibridni pogon</option>
+                    </select>
+                    <select id="pogon" name="pogon">
+                        <option value="Prednji" <?php if (Input::get('pogon') === 'Prednji') echo 'selected'; ?>>Prednji</option>
+                        <option value="Zadnji" <?php if (Input::get('pogon') === 'Zadnji') echo 'selected'; ?>>Zadnji</option>
+                        <option value="4x4" <?php if (Input::get('pogon') === '4x4') echo 'selected'; ?>>4x4</option>
+                    </select>
+                    <select id="menjac" name="menjac">
+                        <optgroup label="Manuelni menjač">
+                            <option value="Manuelni 1 brzina" <?php if (Input::get('menjac') === 'Manuelni 1 brzina') echo 'selected'; ?>>1 brzina</option>
+                            <option value="Manuelni 2 brzina" <?php if (Input::get('menjac') === 'Manuelni 2 brzina') echo 'selected'; ?>>2 brzine</option>
+                            <option value="Manuelni 3 brzina" <?php if (Input::get('menjac') === 'Manuelni 3 brzina') echo 'selected'; ?>>3 brzine</option>
+                            <option value="Manuelni 4 brzina" <?php if (Input::get('menjac') === 'Manuelni 4 brzina') echo 'selected'; ?>>4 brzina</option>
+                            <option value="Manuelni 5 brzina" <?php if (Input::get('menjac') === 'Manuelni 5 brzina') echo 'selected'; ?>>5 brzina</option>
+                            <option value="Manuelni 6 brzina" <?php if (Input::get('menjac') === 'Manuelni 6 brzina') echo 'selected'; ?>>6 brzina</option>
+                        </optgroup>
+                        <optgroup label="Automatski menjač">
+                            <option value="Automatski 1 brzina" <?php if (Input::get('menjac') === 'Automatski 1 brzina') echo 'selected'; ?>>1 brzina</option>
+                            <option value="Automatski 2 brzina" <?php if (Input::get('menjac') === 'Automatski 2 brzina') echo 'selected'; ?>>2 brzine</option>
+                            <option value="Automatski 3 brzina" <?php if (Input::get('menjac') === 'Automatski 3 brzina') echo 'selected'; ?>>3 brzine</option>
+                            <option value="Automatski 4 brzina" <?php if (Input::get('menjac') === 'Automatski 4 brzina') echo 'selected'; ?>>4 brzina</option>
+                            <option value="Automatski 5 brzina" <?php if (Input::get('menjac') === 'Automatski 5 brzina') echo 'selected'; ?>>5 brzina</option>
+                            <option value="Automatski 6 brzina" <?php if (Input::get('menjac') === 'Automatski 6 brzina') echo 'selected'; ?>>6 brzina</option>
+                        </optgroup>
+                    </select>
+                    <input type="text" placeholder="Kubikaza od" name="kubikaza_od" class="search-input" value="<?php echo Input::get('kubikaza_od', ''); ?>" />
+                    <input type="text" placeholder="Kubikaza do" name="kubikaza_do" class="search-input" value="<?php echo Input::get('kubikaza_do', ''); ?>" />
+                    <input type="text" placeholder="Snaga od" name="snaga_od" class="search-input" value="<?php echo Input::get('snaga_od', ''); ?>" />
+                    <input type="text" placeholder="Snaga do" name="snaga_do" class="search-input" value="<?php echo Input::get('snaga_do', ''); ?>" />
+                    <button type="submit" class="login-btn">Pretraži</button>
+                </form>
                 </div>
             </section>
             <section class="car-ads">
@@ -113,6 +210,9 @@ $oglasi = $db->query('SELECT * FROM oglasi WHERE admin_id is NOT NULL limit ? , 
                                 echo '</a>';
                             echo '</div>';
                         }
+                    }
+                    else {
+                        echo "Nema oglasa";
                     }
                     ?>
                 </div>
